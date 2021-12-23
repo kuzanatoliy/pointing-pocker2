6a78f2e04061dbfa249bf36602469f0466b3065e
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2021-12-23 18:06:48 +0300

    feat: init sequelize configuration

---
+++ packages/back-end/src/database/config.d.ts
@@ -0,0 +1,8 @@
+import { Dialect } from 'sequelize';
+
+export declare const database: string;
+export declare const dialect: Dialect;
+export declare const host: string;
+export declare const password: string;
+export declare const port: number;
+export declare const username: string;
---
+++ packages/back-end/src/database/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  database: process.env.DB_NAME,
+  dialect: process.env.DB_DIALECT,
+  host: process.env.DB_HOST,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+};
