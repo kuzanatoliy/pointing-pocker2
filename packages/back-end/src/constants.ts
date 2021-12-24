9c5c8ecbfae0f583bc5bc9b63000fc3441f80263
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2021-12-24 14:41:59 +0300

    feat: init server [PP2-09]

---
+++ packages/back-end/src/constants.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.APP_NAME || 'Pointing pocker 2';
+export const APP_PORT = process.env.APP_PORT || 8000;
+export const APP_SECRET = process.env.APP_SECRET || 'Pointing pocker secret';
--- packages/back-end/src/index.ts
+++ packages/back-end/src/index.ts
@@ -1,1 +1,25 @@
-console.log("Back-end");
+
+import * as http from 'http';
+import * as bodyParser from 'body-parser';
+import session from 'express-session';
+import cors from 'cors';
+
+import { APP_PORT, APP_SECRET } from './constants';
+
+import { Application } from './Application';
+
+const middlewares = [
+  cors(),
+  bodyParser.json(),
+  session({
+    resave: true,
+    saveUninitialized: true,
+    secret: APP_SECRET,
+  }),
+];
+
+const server = http.createServer(new Application(middlewares, []));
+
+server.listen(APP_PORT, () => {
+  console.log(`Server run on ${ APP_PORT } port`);
+});
