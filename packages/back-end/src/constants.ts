a22a942b5ab91aa515c1ca0b76058779af1cebb6
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2021-12-24 16:18:04 +0300

    feat: add google configuration variable [PP2-10]

--- packages/back-end/src/constants.ts
+++ packages/back-end/src/constants.ts
@@ -1,3 +1,9 @@
 export const APP_NAME = process.env.APP_NAME || 'Pointing pocker 2';
 export const APP_PORT = process.env.APP_PORT || 8000;
 export const APP_SECRET = process.env.APP_SECRET || 'Pointing pocker secret';
+
+export const GOOGLE_NAME = process.env.GOOGLE_NAME;
+export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+export const GOOGLE_CALLBACK_URL = process.env.GOOGLE_CALLBACK_URL;
+export const GOOGLE_SCOPE = process.env.GOOGLE_SCOPE;
